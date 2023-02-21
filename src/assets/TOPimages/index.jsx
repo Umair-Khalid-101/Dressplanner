00f067fd5d4a4804b729e32f74fbe5baf5dbc33a
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-21 05:20:32 +0500

    LandingPage

---
+++ src/assets/TOPimages/index.jsx
@@ -0,0 +1,27 @@
+import Girl1 from "./Girl1.jpg";
+import Girl1R from "./Girl1-R.jpg";
+import Girl2 from "./Girl2.jpg";
+import Girl2R from "./Girl2-R.jpg";
+import Girl3 from "./Girl3.jpg";
+import Girl3R from "./Girl3-R.jpg";
+import Girl4 from "./Girl4.jpg";
+import Girl5 from "./Girl5.jpg";
+import Girl6 from "./Girl6.jpg";
+import Girl7 from "./Girl7.jpg";
+import Girl8 from "./Girl8.jpg";
+import Girl9 from "./Girl9.jpg";
+
+export {
+  Girl1,
+  Girl2,
+  Girl3,
+  Girl4,
+  Girl5,
+  Girl6,
+  Girl7,
+  Girl8,
+  Girl9,
+  Girl1R,
+  Girl2R,
+  Girl3R,
+};
--- src/components/AnimatedPics.jsx
+++ src/components/AnimatedPics.jsx
@@ -1,9 +1,65 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+import { react } from "../assets";
+import { Girl1R, Girl2R, Girl3R } from "../assets/TOPimages";
+
 const AnimatedPics = () => {
   return (
-    <motion.div className="flex justify-end items-end">AnimatedPics</motion.div>
+    <div
+      className="flex justify-end items-end h-screen
+    min-w-full"
+    >
+      <motion.div
+        initial={{
+          y: 180,
+          x: -250,
+        }}
+        animate={{
+          x: [-100, 100, -400],
+          y: [200, -200, -550],
+        }}
+        transition={{ duration: 5 }}
+        className="mb-[200px] mr-[200px]
+        w-[160px] h-[120px] rounded-[20px] bg-slate-500 overflow-hidden"
+      >
+        <img src={Girl1R} alt="pic" className="w-full h-full" />
+      </motion.div>
+      {/* <motion.div
+        initial={{
+          y: 0,
+          scale: 2,
+          x: 250,
+        }}
+        animate={{
+          y: -350,
+          scale: 1.5,
+          x: -250,
+        }}
+        transition={{ duration: 3 }}
+        className="bg-slate-500 border-1 rounded-lg h-40 w-60
+        flex justify-center items-center mb-[300px]"
+      >
+        <img src={Girl1R} alt="pic" className="w-full h-full rounded-lg" />
+      </motion.div>
+      <motion.div
+        initial={{
+          y: 0,
+          scale: 1,
+          x: -400,
+        }}
+        animate={{
+          y: -320,
+          scale: 2,
+          x: 0,
+        }}
+        transition={{ duration: 3 }}
+        className="bg-slate-500 border-1 rounded-lg h-40 w-60
+        flex justify-center items-center mr-[200px]"
+      >
+        <img src={Girl2R} alt="pic" className="w-full h-full rounded-lg" />
+      </motion.div> */}
+    </div>
   );
 };
 
